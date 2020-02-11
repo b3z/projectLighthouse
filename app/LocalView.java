a1d5f36253ec14f09395bd6607b4dc1b2112d5d9
b3z <mayer@zeppel.eu>
2020-02-11 15:46:31 +0100

    gerüst localView

---
+++ app/LocalView.java
@@ -0,0 +1,26 @@
+package app;
+import javax.swing.*;
+import app.GridPiece;
+import app.Model;
+
+/**
+ * The local view on our computer. Aka. the GUI the users sees on his pc.
+ * Displays the game.
+ */
+class LocalView extends View {
+
+    /** The frame we operate on. Aka window. */
+    private JFrame frame;
+    /** The grid pieces the game grid is built from. */
+    private GridPiece [] gridPieces;
+
+    /**
+     * Updates the Game UI.
+     * 
+     * @param model the Model
+     */
+    @Override
+    private void update(Model model) {
+        //TODO implement
+    }
+}
