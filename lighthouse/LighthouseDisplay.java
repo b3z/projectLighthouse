7f02f0188e879a598ab5ebad7790ee1a04119ed1
Felioh <felix-eutin@gmx.de>
2020-02-11 15:24:08 +0100

    Luca ist kacka

--- lighthouse/LighthouseDisplay.java
+++ lighthouse/LighthouseDisplay.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+
 import javax.management.InvalidAttributeValueException;
 
 import org.eclipse.jetty.util.ssl.SslContextFactory;
