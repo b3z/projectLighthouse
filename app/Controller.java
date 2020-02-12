87582b4453bdd51dedfc8d17629c2f6217a43336
Felioh <felix-eutin@gmx.de>
2020-02-12 13:18:41 +0100

    Controller, aber fürn arsch

--- app/Controller.java
+++ app/Controller.java
@@ -1,24 +1,31 @@
 package app;
 
+import acm.graphics.GCanvas;
 import acm.program.GraphicsProgram;
 import java.awt.event.*;
 
-public class Controller implements ActionListener {
+public class Controller extends GraphicsProgram {
 
     private Model model;
-    private GraphicsProgram frame;
+    private GCanvas frame;
 
 
-    public Controller(Model model, GraphicsProgram frame) {
+    public Controller(Model model, GCanvas frame) {
         this.model = model;
         this.frame = frame;
 
+        frame.addKeyListener(this);
 
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-
+    public void keyPressed(KeyEvent e) {
+        switch(e.getKeyCode()) {
+            case KeyEvent.VK_LEFT:
+                model.changeColumn(Direction.LEFT);
+                break;
+            case KeyEvent.VK_RIGHT:
+                model.changeColumn(Direction.RIGHT);
+                break;
+        }
     }
 }
