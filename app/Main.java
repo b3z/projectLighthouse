09fc106dacac9c5984046c645b36b858457f84bc
b3z <mayer@zeppel.eu>
2020-02-12 14:24:02 +0100

    handle frame size dynamically

--- app/Main.java
+++ app/Main.java
@@ -1,20 +1,27 @@
 package app;
 
+import java.awt.Dimension;
+
 import acm.program.GraphicsProgram;
+import apple.laf.JRSUIConstants.Widget;
 
 class Main extends GraphicsProgram {
 
     // in tokens.
-    private final int WIDTH = 14;
-    private final int HEIGHT = 28;
+    private final int WIDTH = 6;
+    private final int HEIGHT = 7;
 
     public void init() {
         //create the views.
         LocalView localView = new LocalView(WIDTH, HEIGHT);
         LighthouseView lighthouseView = new LighthouseView();
 
+        
         //add the local view.
         add(localView);
+        
+        //Sets the frame size to the board size (also calculating the height of the frame titlebar.)
+        this.setSize(WIDTH * GridPiece.SIZE, HEIGHT * GridPiece.SIZE + this.getInsets().top);
 
         //create the model.
         Model model = new Model(localView, lighthouseView, WIDTH, HEIGHT);
