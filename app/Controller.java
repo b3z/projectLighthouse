c524ae65b1367529f033c3fa2619c497ad5fbcc5
b3z <mayer@zeppel.eu>
2020-02-17 15:51:36 +0100

    implement toggle

--- app/Controller.java
+++ app/Controller.java
@@ -1,41 +1,44 @@
 package app;
 
 import acm.program.GraphicsProgram;
+import app.Views.MenuView;
 
 import java.awt.event.*;
 
 public class Controller implements KeyListener {
 
     private Model model;
-    private GraphicsProgram frame;
+    private Main frame;
 
 
-    public Controller(Model model, GraphicsProgram frame) {
+    public Controller(Model model, Main frame) {
         this.model = model;
         this.frame = frame;
 
         frame.addKeyListeners(this);
+        //frame.getMenuView().addKeyListener(this);
 
     }
 
     public void keyPressed(KeyEvent e) {
         // System.out.println(e);
         switch(e.getKeyCode()) {
-            case KeyEvent.VK_LEFT:
-                model.changeColumn(Direction.LEFT);
-                // System.out.println("Left");
+            case KeyEvent.VK_LEFT: model.changeColumn(Direction.LEFT);
                 break;
-            case KeyEvent.VK_RIGHT:
-                model.changeColumn(Direction.RIGHT);
-                // System.out.println("Right");
+
+            case KeyEvent.VK_RIGHT: model.changeColumn(Direction.RIGHT);
                 break;
-            case KeyEvent.VK_ENTER:
-            case KeyEvent.VK_SPACE:
-                model.placeToken();
-                // System.out.println("Token placed");
+
+            case KeyEvent.VK_ENTER: 
+            case KeyEvent.VK_SPACE: model.placeToken();
                 break;
-            case KeyEvent.VK_S:
-                model.SaveGame();
+
+            case KeyEvent.VK_S: model.SaveGame();
+                break;
+
+            case KeyEvent.VK_ESCAPE: this.frame.toggleMenu(); // toggeli tiggeli dooo
+                break;
+
         }
     }
 
